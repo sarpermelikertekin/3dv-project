b06ab4667ced9463568f354b59aaf3cbb90aeb50
sarpermelikertekin <sakarama1@hotmail.com>
2023-05-09 12:57:53 +0200

    Finalized

--- Communicaiton over TCP/Assets/Scripts/HololensCameraCapture.cs
+++ Communicaiton over TCP/Assets/Scripts/HololensCameraCapture.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Linq;
 using UnityEngine.Windows.WebCam;
 using UnityEngine.UI;
+using System.Net.Sockets;
+using System;
 
 public class HololensCameraCapture : MonoBehaviour
 {
@@ -11,21 +13,23 @@ public class HololensCameraCapture : MonoBehaviour
 
     public RawImage rawImage;
 
+    public bool sendOnlyImage;
+    private TcpClient client;
+    private NetworkStream stream;
+    private bool isWaitingForResponse = false;
+    private string response;
+
+    public string host = "127.0.0.1";
+    public int port = 5000;
+
     // Use this for initialization
     void Start()
     {
-        StartCoroutine(CaptureRepeatadly());
-    }
-
-    IEnumerator CaptureRepeatadly()
-    {
-        yield return new WaitForSeconds(1);
-
         Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
         targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
 
-        // Create a PhotoCapture object
-        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject)
+        {
             photoCaptureObject = captureObject;
             CameraParameters cameraParameters = new CameraParameters();
             cameraParameters.hologramOpacity = 0.0f;
@@ -34,13 +38,24 @@ public class HololensCameraCapture : MonoBehaviour
             cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
 
             // Activate the camera
-            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
-                // Take a picture
-                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result)
+            {
+                StartCoroutine(CaptureAndSendImage());
             });
         });
+    }
 
-        StartCoroutine(CaptureRepeatadly());
+    IEnumerator CaptureAndSendImage()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+
+            if (!isWaitingForResponse)
+            {
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            }
+        }
     }
 
     void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
@@ -51,14 +66,54 @@ public class HololensCameraCapture : MonoBehaviour
         rawImage.texture = targetTexture;
         rawImage.material.mainTexture = targetTexture;
 
-        // Deactivate the camera
-        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+        // Encode the texture as a PNG image
+        byte[] imageBytes = targetTexture.EncodeToPNG();
+
+        // Prepend the length of the image data as a 4-byte integer
+        byte[] lengthBytes = BitConverter.GetBytes(imageBytes.Length);
+        byte[] sendBytes = new byte[lengthBytes.Length + imageBytes.Length];
+        lengthBytes.CopyTo(sendBytes, 0);
+        imageBytes.CopyTo(sendBytes, lengthBytes.Length);
+
+        if (client == null)
+        {
+            client = new TcpClient(host, port);
+            stream = client.GetStream();
+        }
+
+        // Send the length-prefixed image data over the network
+        stream.Write(sendBytes, 0, sendBytes.Length);
+
+        Debug.Log("Image sent. Length: " + imageBytes.Length);
+
+        // Wait for a response from the server
+        isWaitingForResponse = true;
+        StartCoroutine(WaitForResponse());
     }
 
-    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    IEnumerator WaitForResponse()
     {
-        // Shutdown the photo capture resource
-        photoCaptureObject.Dispose();
-        photoCaptureObject = null;
+        byte[] responseBytes = new byte[1024];
+        int bytesRead = 0;
+        while (bytesRead == 0)
+        {
+            if (stream.DataAvailable)
+            {
+                bytesRead = stream.Read(responseBytes, 0, responseBytes.Length);
+                response = System.Text.Encoding.ASCII.GetString(responseBytes, 0, bytesRead);
+            }
+            yield return null;
+        }
+        isWaitingForResponse = false;
+        Debug.Log("Response received: " + response);
+    }
+
+    void OnApplicationQuit()
+    {
+        void OnApplicationQuit()
+        {
+            stream.Close();
+            client.Close();
+        }
     }
 }
