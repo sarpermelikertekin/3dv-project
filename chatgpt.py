04454ff37ca66d32b27914a9245e5d6dcd5adb0f
Orhun Görkem <gorkem.orhun98@gmail.com>
2023-06-01 20:56:55 +0200

    -

--- chatgpt.py
+++ chatgpt.py
@@ -5,40 +5,67 @@ import pickle
 import numpy as np
 import struct ## new
 import zlib
+import time
 
-HOST='localhost'
-PORT=6000
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print('Socket created')
 
-s.bind((HOST,PORT))
-print('Socket bind complete')
-s.listen(10)
-print('Socket now listening')
+def receive_frame(client_socket, frame_size):
+    frame_data = b""
+    bytes_received = 0
 
-conn,addr=s.accept()
+    print("frame size: ", frame_size)
 
-data = b""
-payload_size = struct.calcsize(">L")
-print("payload_size: {}".format(payload_size))
-while True:
-    while len(data) < payload_size:
-        print("Recv: {}".format(len(data)))
-        data += conn.recv(4096)
+    while bytes_received < frame_size:
+        try:
+            chunk = client_socket.recv(min(frame_size - bytes_received, 8388608))
+            if not chunk:
+                break
+            frame_data += chunk
+            bytes_received += len(chunk)
 
-    print("Done Recv: {}".format(len(data)))
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack(">L", packed_msg_size)[0]
-    print("msg_size: {}".format(msg_size))
-    while len(data) < msg_size:
-        data += conn.recv(4096)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
+        except:
+            return frame_data
 
-    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-    cv2.imwrite("image6.png", frame, [int(cv2.IMWRITE_PNG_COMPRESSION),0]) 
-    #cv2.imshow('ImageWindow',frame)
-    #cv2.waitKey(1)
+    return frame_data
+
+
+def main():
+    server_ip = ''  # Change this to your server IP
+    server_port = 5000  # Change this to your server port
+
+    # Create a TCP socket server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(1)
+
+    print('Waiting for client connection...')
+    client_socket, address = server_socket.accept()
+    print('Client connected:', address)
+
+    while True:
+       
+        size_data = client_socket.recv(4)
+        frame_size = struct.unpack('!I', size_data)[0]
+
+        # Receive the frame from the client
+        frame_data = receive_frame(client_socket, frame_size)
+
+        # Process the received frame as needed
+        with open('received_frame.jpg', 'wb') as f:
+            f.write(frame_data)
+
+        print('Frame received successfully!')
+
+        # Close the client socket
+        client_socket.send("Image received".encode())
+        #print('sleep')
+        #time.sleep(0.1)
+    
+    
+    client_socket.close()
+
+    # Close the server socket
+    server_socket.close()
+
+
+main()
